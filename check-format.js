66e1cd7722c72302bb249aa0b502e718468cbfb7
Hannah Bollar <github@hannahbollar.com>
2023-11-14 10:33:25 -0800

    trigger docs update for folder change (#111)
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

---
+++ check-format.js
@@ -0,0 +1,5 @@
+function_a # Execute Function A
+exit_code_function_a=$? # Capture the exit code of Function A
+
+function_b # Execute Function B
+exit_code_function_b=$? # Capture the exit code of Function B
