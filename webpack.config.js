d29c32398da090f837176b3bf30b5a0a41b84049
Hannah Bollar <github@hannahbollar.com>
2023-11-14 20:35:08 -0800

    testing runs finally properly
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,81 @@
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import webpack from 'webpack';
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import webpack from 'webpack'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const isTesting = process.env.TEST;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default {
-  entry: './src/index.js',
-  output: {
-    publicPath: 'auto',
-    filename: 'build.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'auto',
-    libraryTarget: 'window',
-  },
-
-  devServer: {
-    https: true,
-    static: {
-      directory: path.join(__dirname, 'dist'),
+    entry: './src/index.js',
+    output: {
+        publicPath: 'auto',
+        filename: 'build.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'auto',
+        libraryTarget: 'window',
     },
-    client: {
-      overlay: {
-        errors: true,
-        warnings: false,
-        runtimeErrors: true,
-      },
-    },
-    compress: true,
-  },
 
-  optimization: {
-    minimize: false,
-  },
-
-  experiments: {
-    asyncWebAssembly: true,
-  },
-
-  resolve: {
-    extensions: ['.mjs', '.js'],
-    alias: {
-      MRJS: path.resolve(__dirname, 'src/utils/global'), // <-- When you build or restart dev-server, you'll get an error if the path to your utils.js file is incorrect.
-    },
-    fallback: {
-      fs: false,
-      path: false,
-    },
-    fullySpecified: false, // disable required .js / .mjs when importing
-  },
-
-  mode: process.env.NODE_ENV || 'development',
-
-  plugins: [
-    // ...
-
-    new webpack.ProvidePlugin({
-      MRJS: 'MRJS',
-    }),
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        exclude: isTesting ? undefined : /\.test\.js$/,
-        type: 'javascript/auto',
-      },
-      {
-        test: /\.m?js/,
-        resolve: {
-          fullySpecified: false,
+    devServer: {
+        https: true,
+        static: {
+            directory: path.join(__dirname, 'dist'),
         },
-      },
-      {
-        test: /\.wasm$/,
-        type: 'webassembly/async', // or 'webassembly/sync'
-      },
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false,
+                runtimeErrors: true,
+            },
+        },
+        compress: true,
+    },
+
+    optimization: {
+        minimize: false,
+    },
+
+    experiments: {
+        asyncWebAssembly: true,
+    },
+
+    resolve: {
+        extensions: ['.mjs', '.js'],
+        alias: {
+            MRJS: path.resolve(__dirname, 'src/utils/global'), // <-- When you build or restart dev-server, you'll get an error if the path to your utils.js file is incorrect.
+        },
+        fallback: {
+            fs: false,
+            path: false,
+        },
+        fullySpecified: false, // disable required .js / .mjs when importing
+    },
+
+    mode: process.env.NODE_ENV || 'development',
+
+    plugins: [
+        // ...
+
+        new webpack.ProvidePlugin({
+            MRJS: 'MRJS',
+        }),
     ],
-  },
-};
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js/,
+                type: 'javascript/auto',
+            },
+            {
+                test: /\.m?js/,
+                resolve: {
+                    fullySpecified: false,
+                },
+            },
+            {
+                test: /\.wasm$/,
+                type: 'webassembly/async', // or 'webassembly/sync'
+            },
+        ],
+    },
+}
