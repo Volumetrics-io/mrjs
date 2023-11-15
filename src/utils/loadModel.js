684199831ea8cdead36d5b88e4dd537920918c05
Hannah Bollar <github@hannahbollar.com>
2023-11-15 15:53:25 -0800

    visual check glb
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- src/utils/loadModel.js
+++ src/utils/loadModel.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
 // import { AMFLoader } from 'three/addons/loaders/AMFLoader.js';
 // import { BVHLoader } from 'three/addons/loaders/BVHLoader.js';
 // import { ColladaLoader } from 'three/addons/loaders/ColladaLoader.js';
