f35b4911cfbda9ff22d8365f140e953c75b24688
Hannah Bollar <github@hannahbollar.com>
2023-11-14 22:37:25 -0800

    testing runs, loading works, one filepath issue, temp-save
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- __tests__/loadModel.test.js
+++ __tests__/loadModel.test.js
@@ -1,60 +1,46 @@
+// import { loadModel, abc } from '../src/utils/loadModel';
+// import { jest } from '@jest/globals';
+// import fs from 'fs';
+// import path from 'path';
+// const __dirname = path.resolve();
+
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(abc(1, 2)).toBe(3);
+// });
+
+// test('checkLoadModel - stl', async () => {
+//     const filePath = path.resolve(__dirname, '..', 'dist', 'assets', 'models', 'logo.stl');
+
+//     // Using async/await to handle potential asynchronous operations in loadModel
+//     await expect(async () => {
+//         await loadModel(filePath, 'stl');
+//       }).not.toThrow();
+// });
+
+
+
+
+
+
+
 import { loadModel, abc } from '../src/utils/loadModel';
-import { jest } from '@jest/globals';
 import fs from 'fs';
+import path from 'path';
+
+// Avoid reassigning __dirname, it's a special Node.js variable - using currDir instead.
+const currentDir = 'file://' + process.cwd();
+console.log(currentDir);
 
 test('adds 1 + 2 to equal 3', () => {
     expect(abc(1, 2)).toBe(3);
 });
 
-// TODO - make this not a relative path
-
-// Importing path module to resolve the file path
-import path from 'path';
-
-// Constructing the filePath to logo.stl
-
-function checkLoadModel(filePath, extension) {
-    expect(() => {
-        loadModel(filePath, extension);
-      }).not.toThrow();
-}
-
 test('checkLoadModel - stl', async () => {
-        // console.log('__dirname:', __dirname);
-        const filePath = path.resolve(__dirname, '..', 'dist', 'assets', 'models', 'logo.stl');
-            console.log('Resolved path:', filePath);
+    const filePath = 'file://' + process.cwd() + 'dist/assets/models/logo.stl';
 
-    // loadModel('../dist/assets/models/logo.stl', 'stl');
-    // const usingPath = path.resolve(process.cwd(), 'dist', 'assets', 'models', 'logo.stl');
-
-    // Using async/await to handle potential asynchronous operations in loadModel
-    await expect(async () => {
-        await loadModel('../dist/assets/models/logo.stl', 'stl');
-      }).not.toThrow();
-
-
-
-    // try {
-    //     await loadModel('../dist/assets/models/logo.stl', 'stl');
-    // } catch (error) {
-    //     // Handle any potential error thrown by loadModel
-    //     console.error('Error loading model:', error);
-    //     expect(error).toBeUndefined(); // Optionally, add an assertion to fail the test on error
-    // }
-});
-test('checkLoadModel - gltf', () => {
-    const usingPath = path.resolve(process.cwd(), 'dist', 'assets', 'models', 'logo.gltf');
-    checkLoadModel(usingPath, "gltf");
-});
-test('checkLoadModel - glb', () => {
-    const usingPath = path.resolve(process.cwd(), 'dist', 'assets', 'models', 'logo.glb');
-    checkLoadModel(usingPath, "glb");
-});
-test('checkLoadModel - usd', () => {
-    const usingPath = path.resolve(process.cwd(), 'dist', 'assets', 'models', 'logo.usd');
-    checkLoadModel(usingPath, "usd");
-});
-test('checkLoadModel - usdz', () => {
-    const usingPath = path.resolve(process.cwd(), 'dist', 'assets', 'models', 'logo.usdz');
-    checkLoadModel(usingPath, "usdz");
+    try {
+        await loadModel(filePath, 'stl');
+    } catch (error) {
+        throw error;
+    }
 });
--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,3 @@
-export default { transform: {} }
+export default { 
+    transform: {}
+}
