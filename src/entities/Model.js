6e8bb8d708dc104d1022e347937472bb1a8cc163
Hannah Bollar <github@hannahbollar.com>
2023-11-14 22:45:15 -0800

    quick update, still based on last temp save
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- src/entities/Model.js
+++ src/entities/Model.js
@@ -1,65 +1,64 @@
-import Entity from '../core/entity'
-import { loadModel } from '../utils/loadModel'
+import Entity from '../core/entity';
+import { loadModel } from '../utils/loadModel';
 
 /**
  *
  */
 export class Model extends Entity {
-    /**
-     *
-     */
-    constructor() {
-        super()
+  /**
+   *
+   */
+  constructor() {
+    super();
+  }
+
+  /**
+   *
+   */
+  get height() {
+    super.height; // TODO - why is this line here?
+    return this.contentHeight;
+  }
+
+  /**
+   *
+   */
+  connected() {
+    this.src = this.getAttribute('src');
+    if (!this.src) {
+      return;
     }
 
-    /**
-     *
-     */
-    get height() {
-        super.height // TODO - why is this line here?
-        return this.contentHeight
-    }
+    const extension = this.src.slice(((this.src.lastIndexOf('.') - 1) >>> 0) + 2);
 
-    /**
-     *
-     */
-    connected() {
-        this.src = this.getAttribute('src');
-        if (!this.src) {
-            return;
-        }
+    // todo - these material changes should be moved out of the loader at some point
+    const material = new THREE.MeshPhysicalMaterial({
+      clearcoat: 0.75,
+      clearcoatRoughness: 0.5,
+      metalness: 0.5,
+      roughness: 0.6,
+      envMapIntensity: 0.75,
+    });
 
-        const extension = this.src.slice(((this.src.lastIndexOf('.') - 1) >>> 0) + 2);
-
+    loadModel(this.src, extension)
+      .then((loadedMeshModel) => {
         // todo - these material changes should be moved out of the loader at some point
-        const material = new THREE.MeshPhysicalMaterial({
-            clearcoat: 0.75,
-            clearcoatRoughness: 0.5,
-            metalness: 0.5,
-            roughness: 0.6,
-            envMapIntensity: 0.75,
-        });
+        loadedMeshModel.material = material;
+        loadedMeshModel.receiveShadow = true;
+        loadedMeshModel.renderOrder = 3;
 
-        loadModel(this.src, extension)
-            .then((loadedMeshModel) => {
-                 // todo - these material changes should be moved out of the loader at some point
-                loadedMeshModel.material = material;
-                loadedMeshModel.receiveShadow = true;
-                loadedMeshModel.renderOrder = 3;
+        // the below is the same as 'scene.add'
+        this.object3D.add(loadedMeshModel);
 
-                // the below is the same as 'scene.add'
-                this.object3D.add(loadedMeshModel);
+        // TODO - recheck this lower part
+        this.dispatchEvent(new CustomEvent('new-entity', { bubbles: true }));
+      })
+      .catch((error) => {
+        console.log(`ERR: in loading model ${this.src}. Error was:`, error);
+      });
+  }
 
-                // TODO - recheck this lower part
-                this.dispatchEvent(new CustomEvent('new-entity', { bubbles: true }));
-            })
-            .catch((error) => {
-                console.log(`ERR: in loading model ${this.src}. Error was:`, error);
-                return;
-            });
-    }
-
-    onLoad = () => {}
+  onLoad = () => {};
 }
 
-customElements.get('mr-model') || customElements.define('mr-model', Model)
+customElements.get('mr-model') || customElements.define('mr-model', Model);
