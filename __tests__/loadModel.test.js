b6c619ad5cf32e27fb49aeedbea01f7931c59157
Hannah Bollar <github@hannahbollar.com>
2023-11-16 20:01:56 -0800

    cleanup
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- __tests__/loadModel.test.js
+++ __tests__/loadModel.test.js
@@ -1,28 +1,3 @@
-// import { loadModel, abc } from '../src/utils/loadModel';
-// import { jest } from '@jest/globals';
-// import fs from 'fs';
-// import path from 'path';
-// const __dirname = path.resolve();
-
-// test('adds 1 + 2 to equal 3', () => {
-//     expect(abc(1, 2)).toBe(3);
-// });
-
-// test('checkLoadModel - stl', async () => {
-//     const filePath = path.resolve(__dirname, '..', 'dist', 'assets', 'models', 'logo.stl');
-
-//     // Using async/await to handle potential asynchronous operations in loadModel
-//     await expect(async () => {
-//         await loadModel(filePath, 'stl');
-//       }).not.toThrow();
-// });
-
-
-
- 
-
-
-
 import { loadModel } from '../src/utils/loadModel';
 
 // todo: in future dont hard code this, but the relative links based on filepath dont work
