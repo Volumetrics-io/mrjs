2c5139f68669c9a825da46913fcccd68180d69c8
Hannah Bollar <github@hannahbollar.com>
2023-11-20 10:55:07 -0800

    cleanup
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- src/UI/Text/Text.js
+++ src/UI/Text/Text.js
@@ -6,40 +6,38 @@ import { MRUIEntity } from '../UIEntity';
  *
  */
 export class MRText extends MRUIEntity {
+    /**
+     *
+     */
+    get height() {
+        super.height;
+        this.aabb.setFromObject(this.textObj).getSize(this.size);
+        return this.size.y;
+    }
 
-  /**
-   *
-   */
-  get height() {
-    super.height;
-    this.aabb.setFromObject(this.textObj).getSize(this.size);
-    return this.size.y;
-  }
+    /**
+     *
+     */
+    constructor() {
+        super();
+        this.textObj = new Text();
+        this.object3D.add(this.textObj);
+        this.editable = false;
 
-  /**
-   *
-   */
-  constructor() {
-    super();
-    this.textObj = new Text();
-    this.object3D.add(this.textObj);
-    this.editable = false;
+        this.needsUpdate = true;
 
-    this.needsUpdate = true;
+        document.addEventListener('container-mutated', () => {
+            this.needsUpdate = true;
+        });
+    }
 
-    document.addEventListener('container-mutated', () => {
-      this.needsUpdate = true;
-    });
-  }
-
-  /**
-   *
-   */
-  connected() {
-    const text = this.textContent.trim();
-    this.textObj.text = text.length > 0 ? text : ' ';
-  }
-
+    /**
+     *
+     */
+    connected() {
+        const text = this.textContent.trim();
+        this.textObj.text = text.length > 0 ? text : ' ';
+    }
 }
 
 customElements.get('mr-text') || customElements.define('mr-text', MRText);
