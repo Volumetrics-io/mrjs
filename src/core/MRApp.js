a18eb9eaebf55365b2376f1117064ec418a3fa57
michael <michael@volumetrics.io>
2023-11-19 16:53:16 -0800

    deleted old textinputsystem.

--- src/core/MRApp.js
+++ src/core/MRApp.js
@@ -92,7 +92,6 @@ export class MRApp extends MRElement {
       this.physicsWorld = new RAPIER.World({ x: 0.0, y: -9.81, z: 0.0 });
       this.physicsSystem = new RapierPhysicsSystem();
       this.controlSystem = new ControlSystem();
-      //this.textInputSystem = new TextInputSystem();
       this.textSystem = new TextSystem();
 
       this.clippingSystem = new ClippingSystem();
