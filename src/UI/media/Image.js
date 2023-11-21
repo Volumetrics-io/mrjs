df434889bf99cf2f003df5fda7f8b3857a604e36
Michael Gutensohn <michael@volumetrics.io>
2023-11-20 23:54:32 -0800

    146 mr img changes the image colors (#149)

--- src/UI/media/Image.js
+++ src/UI/media/Image.js
@@ -11,8 +11,10 @@ export class MRImage extends MRUIEntity {
     constructor() {
         super();
         this.geometry = UIPlane(1, 1, 0.0001, 18);
-        this.material = new THREE.MeshBasicMaterial({
+        this.material = new THREE.MeshStandardMaterial({
             side: 0,
+            receiveShadow: true,
+            renderOrder: 3,
         });
         this.object3D = new THREE.Mesh(this.geometry, this.material);
     }
