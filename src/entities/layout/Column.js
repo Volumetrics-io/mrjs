52589333698afbaf87d4df5e034dbae5ebd69352
Hannah Bollar <github@hannahbollar.com>
2023-11-28 13:05:18 -0800

    change comment to deletion
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- src/entities/layout/Column.js
+++ src/entities/layout/Column.js
@@ -25,10 +25,8 @@ export class Column extends LayoutEntity {
         this.accumulatedY = -this.pxToThree(this.compStyle.paddingTop);
         for (const index in children) {
             const child = children[index];
-            // this.accumulatedY -= this.pxToThree(child.compStyle.marginTop);
             child.object3D.position.setY(this.accumulatedY - child.height / 2);
             this.accumulatedY -= child.height;
-            // this.accumulatedY -= this.pxToThree(child.compStyle.marginBottom);
         }
         this.accumulatedY -= this.pxToThree(this.compStyle.paddingBottom);
         this.shuttle.position.setY(this.parentElement.height / 2);
