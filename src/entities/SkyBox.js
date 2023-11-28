4d51e57de2ad579664df700458ec5961aa2855ad
Hannah Bollar <github@hannahbollar.com>
2023-11-27 17:56:56 -0800

    fix
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- src/entities/SkyBox.js
+++ src/entities/SkyBox.js
@@ -11,21 +11,19 @@ class SkyBox extends Entity {
         // or you can store them in a specified path and just
         // load them up solely by filename in that path.
         
-        this.src = this.getAttribute('src');
+        this.texturesList = this.getAttribute('src');
         if (!this.texturesList) {
             return;
         }
 
         const textureLoader = THREE.CubeTextureLoader();
         const textureNames = texturesList.split(',');
-        let texture = null;
 
         let path = this.getAttribute('pathToTextures');
         const texture = !path ? textureLoader.load(textureNames) : textureLoader.load(textureNames.map((name) => path + name));
 
         // scene.background
         this.object3D.background = texture;
-
     }
 
     onLoad = () => {};
