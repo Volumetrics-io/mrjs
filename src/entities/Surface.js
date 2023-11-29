d97cf0e5242835afb63f1bc4fa099ece8d75a1a5
Hannah Bollar <github@hannahbollar.com>
2023-11-29 12:54:11 -0800

    update for cleanup and some remaining quesitons
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- src/entities/Surface.js
+++ src/entities/Surface.js
@@ -31,6 +31,16 @@ export class Surface extends Entity {
         this.translation.renderOrder = 3;
     }
 
+    setupForMasking(maskedMesh) {
+        maskedMesh.renderOrder = this.stencilMeshes.length + 1;
+        maskedMesh.onBeforeRender = function (renderer) {
+            renderer.state.buffers.stencil.setOp(THREE.KeepStencilOp, THREE.KeepStencilOp, THREE.KeepStencilOp);
+            stencilMeshes.forEach((stencilMesh, index) => {
+                renderer.state.buffers.stencil.setFunc(THREE.EqualStencilFunc, index + 1, 0xff);
+            });
+        };
+    }
+
     /**
      *
      */
@@ -68,8 +78,11 @@ export class Surface extends Entity {
         this.group.visible = true;
         this.viz.visible = false;
 
+        // create once at the beginning - this should not need to be updated.
+        // TODO - should i keep stencil Meshes and this.viz separate..?
         this.stencilMeshes=[this.viz];
-        this.maskedMeshes=this.group;
+        // This will be updated as objects are added/removed from a scene.
+        this.maskedMeshes=this.group; // TODO - check to see if an item is added to a group that it is masked as well?
 
         // Use stencil operation for each stencil object
         stencilMeshes.forEach((stencilMesh, index) => {
@@ -85,13 +98,7 @@ export class Surface extends Entity {
 
         // Apply stencil masking to each masked object - this will include all objects that we want to be viewed as if theyre 'on the screen directly'
         maskedMeshes.forEach((maskedMesh) => {
-            maskedMesh.renderOrder = stencilMeshes.length + 1;
-            maskedMesh.onBeforeRender = function (renderer) {
-                renderer.state.buffers.stencil.setOp(THREE.KeepStencilOp, THREE.KeepStencilOp, THREE.KeepStencilOp);
-                stencilMeshes.forEach((stencilMesh, index) => {
-                    renderer.state.buffers.stencil.setFunc(THREE.EqualStencilFunc, index + 1, 0xff);
-                });
-            };
+            setupForMasking(maskedMesh);
         });
     }
 
@@ -100,7 +107,7 @@ export class Surface extends Entity {
      * @param entity
      */
     add(entity) {
-        this.group.add(entity.object3D);
+        this.group.add(entity.object3D); // TODO - check to see if this enables and disables masking by default
     }
 
     /**
@@ -108,7 +115,7 @@ export class Surface extends Entity {
      * @param entity
      */
     remove(entity) {
-        this.group.remove(entity.object3D);
+        this.group.remove(entity.object3D); // TODO - check to see if this enables and disables masking by default
     }
 
     /**
@@ -131,7 +138,6 @@ export class Surface extends Entity {
      */
     place() {
         this.viz.removeFromParent();
-        this.mask.removeFromParent();
 
         this.group.visible = true;
         this.placed = true;
