c90a290857129e1b33a9c94a14a565a0b4efb205
Michael Gutensohn <michael@volumetrics.io>
2023-12-04 18:37:48 -0800

    fixing minor bug for in headset scrolling.

--- src/entities/layout/Container.js
+++ src/entities/layout/Container.js
@@ -38,7 +38,7 @@ export class Container extends LayoutEntity {
 
         this.parentElement.addEventListener('surface-placed', (event) => {
             this.dispatchEvent(new CustomEvent('container-mutated', { bubbles: true }));
-            this.scrollMax = MRJS.computeBoundingSphere(this.shuttle).radius * 2 - this.height;
+            this.scrollMax = (MRJS.computeBoundingSphere(this.shuttle).radius * 2 * this.windowVerticalScale)// - this.height;
         });
 
         this.parentElement.addEventListener('surface-removed', (event) => {
