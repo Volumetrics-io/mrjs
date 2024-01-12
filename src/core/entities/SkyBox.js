b1352c4cc9e1fdbae9f9214b2790a6c09e1e0652
Michael Gutensohn <michael@volumetrics.io>
2024-01-11 17:22:26 -0800

    reduced rendering fulstrum distance and shrank skybox a bit.

--- src/core/entities/SkyBox.js
+++ src/core/entities/SkyBox.js
@@ -61,7 +61,7 @@ export class SkyBox extends MREntity {
             textureNames.map((name) => (path ? path + name : name)),
             this.onTextureLoaded.bind(this) // Ensuring the correct context
         );
-        const geometry = new THREE.SphereGeometry(1000, 32, 16);
+        const geometry = new THREE.SphereGeometry(900, 32, 16);
 
         this.skybox = new THREE.Mesh(geometry); // going to passively load texture on async
         this.object3D.add(this.skybox);
