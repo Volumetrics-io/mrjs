f563d610b942c199272f1dfadf9389b954d42dc7
Hannah Bollar <github@hannahbollar.com>
2024-01-19 02:48:32 -0800

    add base
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

---
+++ __tests__/samplesTesting.test.js
@@ -0,0 +1,62 @@
+import * as puppeteer from 'puppeteer';
+
+// todo: in future dont hard code this, but the relative links based on filepath dont work
+// using a server to host them works best, so just grabbing from github is fine for now.
+const MODELS_URL = 'https://github.com/Volumetrics-io/mrjs/main/samples/assets/models/'
+
+describe('loadModel Function', () => {
+    let browser;
+    let page;
+    let errors = [];
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch({ headless: "new" });
+        page = await browser.newPage();
+
+        // Listen for console events and record any errors
+        page.on('console', msg => {
+            if (msg.type() === 'error') {
+                errors.push(msg.text());
+            }
+        });
+
+        // Define your HTML content
+        const htmlContent = `
+            <!DOCTYPE html>
+            <html>
+                <meta charset="utf-8">
+                <title>MR.js - TEST PAGE</title>
+                <meta name="description" content="TEST PAGE">
+                <script src="../dist/mr.js"></script>
+                <link rel="stylesheet" type="text/css" href="./style.css" />
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+            <body>
+                <mr-app>
+                    <mr-panel class="layout">
+                        <mr-div>
+                            <mr-model src="${MODELS_URL}logo.glb">
+                            </mr-model>
+                            <mr-model src="${MODELS_URL}logo.stl">
+                            </mr-model>
+                            <mr-model src="${MODELS_URL}logo.fbx">
+                            </mr-model>
+                        </mr-div>
+                    </mr-panel>
+                </mr-app>
+            </body>
+            </html>
+        `;
+
+        // Set the content of the page
+        await page.setContent(htmlContent);
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    test('Page should load with no console errors', async () => {
+        console.log('Console Errors:', errors); // Log errors
+        expect(errors).toHaveLength(0);
+    });
+});
---
+++ __tests__/unitTestingBase.test.js
@@ -0,0 +1,58 @@
+// import fetchMock from 'jest-fetch-mock';
+
+// import { Model } from '../src/utils/Model';
+
+// fetchMock.enableMocks();
+
+// const MRJS_TESTING_SERVER = 'https://localhost:8080';
+
+// // todo: in future dont hard code this, but the relative links based on filepath dont work
+// // using a server to host them works best, so just grabbing from github is fine for now.
+// const MODELS_URL = 'https://github.com/Volumetrics-io/MR.js/blob/main/assets/models/';
+
+// describe('loadModel function tests', () => {
+//   beforeAll(async () => {
+//     page = await browser.newPage();
+//     await page.goto(MRJS_TESTING_SERVER);
+//   });
+
+//   test('checkLoadModel - stl', async () => {
+//     // Mock the network response for STL model
+//     fetchMock.mockResponseOnce('mocked STL model response');
+
+//     // Call your loadModel function with Puppeteer
+//     const response = await page.evaluate(() => 
+//       Model.loadModel('${MODELS_URL}logo.stl', 'stl')
+//     );
+
+//     expect(response).toBe('mocked STL model response'); // Update assertion as per your logic
+//   });
+
+//   test('checkLoadModel - glb', async () => {
+//     // Mock the network response for GLB model
+//     fetchMock.mockResponseOnce('mocked GLB model response');
+
+//     // Call your loadModel function with Puppeteer
+//     const response = await page.evaluate(() => 
+//       Model.loadModel('${MODELS_URL}logo.glb', 'glb')
+//     );
+
+//     expect(response).toBe('mocked GLB model response'); // Update assertion as per your logic
+//   });
+
+//   test('checkLoadModel - fbx', async () => {
+//     // Mock the network response for FBX model
+//     fetchMock.mockResponseOnce('mocked FBX model response');
+
+//     // Call your loadModel function with Puppeteer
+//     const response = await page.evaluate(() => 
+//       Model.loadModel('${MODELS_URL}logo.fbx', 'fbx')
+//     );
+
+//     expect(response).toBe('mocked FBX model response'); // Update assertion as per your logic
+//   });
+
+//   // Additional tests for GLB and FBX...
+// });
+
+// commenting out for now
