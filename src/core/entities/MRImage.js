1e08d009eb93b134f412f80d503476c179da3b2a
Michael Gutensohn <michael@volumetrics.io>
2024-01-23 18:12:21 -0800

    294 mr img png transparency support (#295)

--- src/core/entities/MRImage.js
+++ src/core/entities/MRImage.js
@@ -23,6 +23,8 @@ export class MRImage extends MRDivEntity {
         // until the connected call since this will get overwritten anyways.
         let material = new THREE.MeshStandardMaterial({
             side: 0,
+            transparent: true, 
+            // opacity: 0.5,
         });
         this.object3D = new THREE.Mesh(undefined, material);
         this.object3D.receiveShadow = true;
