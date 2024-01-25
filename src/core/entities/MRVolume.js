cfaeffb0f6e72f06880227eb70cd48eb5ff0cfc2
Michael Gutensohn <michael@volumetrics.io>
2024-01-24 22:09:07 -0800

    tweaked MRVolume to sit above a plane when it's anchored to it.

--- src/core/entities/MRVolume.js
+++ src/core/entities/MRVolume.js
@@ -3,22 +3,21 @@ import { MREntity } from "../MREntity";
 export class MRVolume extends MREntity {
     constructor() {
         super()
-
-        
-
+            this.volume = new THREE.Object3D()
+            this.object3D.add(this.volume)
         }
 
         connected(){
             this.clipping = new MRClippingGeometry(new THREE.BoxGeometry(1, 1, 1));
-        console.log('here');
-        this.ignoreStencil = true
+            this.ignoreStencil = true
 
-        this.addEventListener('anchored', () => {
-            if (this.plane) {
-                let height = this.plane.dimensions.x <= this.plane.dimensions.z ? this.plane.dimensions.x : this.plane.dimensions.z
-                this.clipping.geometry.copy(new THREE.BoxGeometry(this.plane.dimensions.x, height, this.plane.dimensions.z));
-            }
-        })
+            this.addEventListener('anchored', () => {
+                if (this.plane) {
+                    let height = this.plane.dimensions.x <= this.plane.dimensions.z ? this.plane.dimensions.x : this.plane.dimensions.z
+                    this.volume.position.y += height / 2
+                    this.clipping.geometry.copy(new THREE.BoxGeometry(this.plane.dimensions.x, height, this.plane.dimensions.z));
+                }
+            })
             
         }
     }
