1701d5ef6eef9d5a3076fa26a207f4dd6ff8ffa0
Hannah Bollar <github@hannahbollar.com>
2024-01-29 12:13:52 -0800

    update
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- src/core/entities/MRHyperlink.js
+++ src/core/entities/MRHyperlink.js
@@ -15,11 +15,20 @@ export default class MRHyperlink extends MRTextEntity {
         this.object3D.name = 'hyperlink';
     }
 
+    _createLink() {
+        if (!this.link) {
+            this.link = document.createElement('a');
+            this.link.setAttribute('href', (this.getAttribute('href') ?? undefined));
+        }
+    }
+
     get href() {
-        return this.getAttribute('href');
+        this._createLink();
+        return this.link.getAttribute('href');
     }
 
     set href(src_str) {
+        this._createLink();
         this.link.setAttribute('href', src_str);
     }
 
@@ -28,8 +37,7 @@ export default class MRHyperlink extends MRTextEntity {
      */
     connected() {
         super.connected();
-        this.link = document.createElement('a');
-        this.link.setAttribute('href', this.getAttribute('href'));
+        this._createLink();
 
         this.addEventListener('touch-start', () => {
             this.classList.add('active');
