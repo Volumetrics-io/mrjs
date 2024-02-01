e0ec55452d7c327af7dbc047e6277a02b534bcce
Hannah Bollar <github@hannahbollar.com>
2024-01-31 16:52:33 -0800

    add note
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

---
+++ src/core/entities/MRMask.js
@@ -0,0 +1,5 @@
+MRMask.js
+// may be useful as an understanding separate from mr-panel and overflow:hidden
+// otherwise let users lean entirely on overflow: hidden
+
+// need to solve the double masking bug first before determining if this separate file is even necessary though
