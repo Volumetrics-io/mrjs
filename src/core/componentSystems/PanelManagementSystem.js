a964cde09f8ef6c66ec90d186911e60fc5e7b94b
Michael Gutensohn <michael@volumetrics.io>
2024-02-05 23:41:44 -0800

    wrote JS docs.

--- src/core/componentSystems/PanelManagementSystem.js
+++ src/core/componentSystems/PanelManagementSystem.js
@@ -2,17 +2,38 @@ import { MRSystem } from 'mrjs/core/MRSystem';
 import { MRPanel } from '../entities/MRPanel';
 
 
+/**
+ * @class PanelManagementSystem
+ * @classdesc A system that manages the screen relative position of UI panels
+ * @augments MRSystem
+ */
 export class PanelManagementSystem extends MRSystem {
+
+    /**
+     * @class
+     * @description Constructor for the PanelManagementSystem system. Uses the default System setup.
+     */
     constructor() {
         super(false)
     }
 
+    /**
+     * @function
+     * @description The generic system update call. keeps panel positions up to date.
+     * @param {number} deltaTime - given timestep to be used for any feature changes
+     * @param {object} frame - given frame information to be used for any feature changes
+     */
     update(dt, f) {
         for (const entity of this.registry) {
             this.updatePanel(entity);
         }
     }
 
+    /**
+     * @function
+     * @description Called when a new entity is added to this system
+     * @param {MREntity} entity - the entity being added.
+     */
     onNewEntity(entity) {
         if (entity instanceof MRPanel) {
             this.registry.add(entity)
@@ -20,6 +41,11 @@ export class PanelManagementSystem extends MRSystem {
         }
     }
 
+    /**
+     * @function
+     * @description used to set the position of an individual panel
+     * @param {MREntity} entity - the entity being added.
+     */
     updatePanel(entity) {
         const rect = entity.getBoundingClientRect();
         const appRect = this.app.getBoundingClientRect();
