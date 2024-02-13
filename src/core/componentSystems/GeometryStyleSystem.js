c8d3040b3c18f43b7bcb73a66d1057891dd06351
Hannah Bollar <github@hannahbollar.com>
2024-02-13 13:52:43 -0800

    a small fix for the geometry update (#395)
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>
    
    - fixes update bubbling check
    - does a rename for a function and property in mrimage for better readability in geometrysystem

--- src/core/componentSystems/GeometryStyleSystem.js
+++ src/core/componentSystems/GeometryStyleSystem.js
@@ -32,23 +32,31 @@ export class GeometryStyleSystem extends MRSystem {
                 return;
             }
 
+            // Only want to dispatch if anything was actually updated
+            // in this iteration.
+            let changed = false;
+
             // Anything needed for mrjs defined entities - the order of the below matters
             if (entity instanceof MRDivEntity) {
-                this.setBorder(entity);
+                changed = this.setUpdatedBorder(entity);
             }
-            this.setScale(entity);
+            changed = this.setScale(entity);
             if (entity instanceof MRImage) {
-                this.setUpdatedImagePlane(entity);
+                changed = this.setUpdatedImagePlane(entity);
+                changed = this.fixImagePlaneForZIndex(entity);
             }
 
             // User additional - Main Entity Style Change
             if (entity instanceof MREntity) {
-                entity.updateGeometryStyle();
+                changed = entity.updateGeometryStyle();
             }
             
             // Cleanup
-            // FIXME: this is necessary in some cases but it's causing a constant update as of now
-            // entity.dispatchEvent(new CustomEvent('child-updated', { bubbles: true }));
+            if (changed) {
+                // TBH i think this is only needed for scale, but just in case others use changed
+                // width/height for anything else, and update is required for children as well
+                entity.dispatchEvent(new CustomEvent('child-updated', { bubbles: true }));
+            }
             if (!entity.alwaysNeedsGeometryUpdate) {
                 entity.needsGeometryUpdate = false;
             }
@@ -65,28 +73,75 @@ export class GeometryStyleSystem extends MRSystem {
     }
 
     setScale(entity) {
-        entity.object3D.scale.setScalar(
-            entity.compStyle.scale != 'none'
+        let new_scale = entity.compStyle.scale != 'none'
             ? parseFloat(entity.compStyle.scale) * mrjsUtils.app.scale
-            : 1
-        );
+            : 1;
+        if (new_scale != entity.object3D.scale) {
+            entity.object3D.scale.setScalar(new_scale);
+            return true;
+        }
+        return false;
     }
 
     /**
      * @function
      * @description Sets the border of the UI based on compStyle and inputted css elements.
      */
-    setBorder(entity) {
-        entity.background.geometry = mrjsUtils.geometry.UIPlane(entity.width, entity.height, entity.borderRadii, 18);
+    setUpdatedBorder(entity) {
+        // geometry will only update if width, height, or borderRadii have changed
+        let w = entity.width;
+        let h = entity.height;
+        let b = entity.borderRadii;
+        if (entity._storedWidth != w || entity._storedHeight != h || entity._storedBorderRadii != b) {
+            entity._storedWidth = w;
+            entity._storedHeight = h;
+            entity._storedBorderRadii = b;
+        } else {
+            // no update needed
+            return false;
+        }
+
+        if (entity.background.geometry !== undefined) {
+            entity.background.geometry.dispose();
+        }
+        entity.background.geometry = mrjsUtils.geometry.UIPlane(w, h, b, 18);
+
+        return true;
     }
 
     setUpdatedImagePlane(entity) {
-        entity.computeObjectFitDimensions();
+        entity.computeImageObject3DFitDimensions();
 
         // geometry will only update if width, height, or borderRadii have changed
+        let w = entity.width;
+        let h = entity.height;
+        let b = entity.borderRadii;
+        if (entity._storedWidth != w || entity._storedHeight != h || entity._storedBorderRadii != b) {
+            entity._storedWidth = w;
+            entity._storedHeight = h;
+            entity._storedBorderRadii = b;
+        } else {
+            // no update needed
+            return false;
+        }
+
         if (entity.object3D.geometry !== undefined) {
             entity.object3D.geometry.dispose();
         }
-        entity.object3D.geometry = mrjsUtils.geometry.UIPlane(entity.width, entity.height, entity.borderRadii, 18);
+        entity.object3D.geometry = mrjsUtils.geometry.UIPlane(w, h, b, 18);
+
+        return true;
+    }
+
+    fixImagePlaneForZIndex(entity) {
+        // Since we potentially updated the background plane and the image plane itself,
+        // we need to make sure the image plane is bumped forward a little bit
+        //
+        // entity.object3D is the image itself.
+        if (entity.object3D.position.z == entity.background.position.z) {
+            entity.object3D.position.z += 0.0001;
+            return true;
+        }
+        return false;
     }
 }
