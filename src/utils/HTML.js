e940e9c1e43544b9b633cb41fd97d7f2b64466d4
Hannah Bollar <github@hannahbollar.com>
2024-02-27 13:15:24 -0800

    cleanup url queries from main url loaders (#452)
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- src/utils/HTML.js
+++ src/utils/HTML.js
@@ -1,3 +1,5 @@
+// import { URL } from 'url';
+
 /**
  * @namespace html
  * @description Useful namespace for helping with html utility functions
@@ -7,14 +9,28 @@ let html = {};
 /**
  * @function
  * @memberof html
- * @param {string} path - either a relative or full path inputted to an element
+ * @param {string} path - either a relative or full path inputted to an element.
+ * Additionally removes all queries from the end of the url, leaving the input as just the origin and its pathname.
+ * For ex: 'https://example.com/images/photo.png?version=2' becomes 'https://example.com/images/photo.png'
  * @description Given the path returns an absolute path resolved so relative linking works as expected.
  * @returns {string} a.href - the absolute path
  */
-html.resolvePath = function (path) {
+html.resolvePath = function (path, baseUrl = window.location.href) {
     let a = document.createElement('a');
-    a.href = path;
+    a.href = html.removeUrlQueries(path, baseUrl);
     return a.href;
 };
+
+html.removeUrlQueries = function (path, baseUrl) {
+    try {
+        // Check if path is absolute. If not, use baseUrl as the second parameter
+        let urlObj = new URL(path, baseUrl);
+        let cleanUrl = urlObj.origin + urlObj.pathname;
+        return cleanUrl;
+    } catch (error) {
+        console.error("Error processing URL:", error.message);
+        return path; // Return the original path if there's an error
+    }
+}
 
 export { html };
