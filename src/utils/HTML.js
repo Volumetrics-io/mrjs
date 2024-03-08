70052a5b01277d19ef56dc1dd30b4c93bfbab561
Hannah Bollar <github@hannahbollar.com>
2024-03-08 13:19:42 -0800

    update
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- src/utils/HTML.js
+++ src/utils/HTML.js
@@ -19,10 +19,20 @@ html.resolvePath = function (path, baseUrl = window.location.href) {
     return a.href;
 };
 
-html.removeUrlQueries = function (path, baseUrl = window.location.href) {
+/**
+ * @function
+ * @memberof html
+ * @param {string} path - either a relative or full path inputted to an element.
+ * @param {string} baseUrl - 
+ * Additionally removes all queries from the end of the url, leaving the input as just the origin and its pathname.
+ * For ex: 'https://example.com/images/photo.png?version=2' becomes 'https://example.com/images/photo.png'
+ * @description Given the path returns an absolute path resolved so relative linking works as expected.
+ * @returns {string} a.href - the absolute path
+ */
+html.removeUrlQueries = function (path, baseUrl = window.location.origin) {
     try {
         // Check if path is absolute. If not, use baseUrl as the second parameter
-        let urlObj = new URL(path, baseUrl);
+        let urlObj = new URL(path, baseUrl === 'about:srcdoc' ? window.location.origin : baseUrl);
         let cleanUrl = urlObj.origin + urlObj.pathname;
         return cleanUrl;
     } catch (error) {
