53250dcefe95508e8bcfe4306f8e3d479ae433ff
Hannah Bollar <github@hannahbollar.com>
2024-03-21 13:39:57 -0700

    temp save
    Signed-off-by: Hannah Bollar <github@hannahbollar.com>

--- src/core/entities/MRTextField.js
+++ src/core/entities/MRTextField.js
@@ -35,14 +35,6 @@ export class MRTextField extends MRTextEntity {
         this.wrapper.innerHTML = '<slot></slot>';
     }
 
-    get value() {
-        return this.input.value;
-    }
-
-    set value(val) {
-        this.input.value = val;
-    }
-
     /**
      * @function
      * @description Callback function of MREntity - handles setting up this textfield once it is connected to run as an entity component.
